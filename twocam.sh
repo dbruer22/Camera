416a9425482ee0a845d91983efb63856961d7824
Daniel <dbruer22@bsmschool.org>
2020-09-17 10:53:50 -0500

    maybe change to an mp4

--- twocam.sh
+++ twocam.sh
@@ -2,10 +2,9 @@
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-ffmpeg -f video4linux2 -r 25 -s 640x480 -i /dev/video0 black.mp4 \
-       -f video4linux2 -r 25 -s 640x480 -i /dev/video2 blue.mp4 \
-       -map 0 camera1.webm \
-       -map 1 camera2.webm
+ffmpeg -f video4linux2 -r 25 -s 640x480 -i /dev/video0 black1.mp4 \
+       -f video4linux2 -r 25 -s 640x480 -i /dev/video2 blue1.mp4 \
+
 
 
 
