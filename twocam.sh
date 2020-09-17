02401af96577045f07552b037a34c3c07475c240
Daniel <dbruer22@bsmschool.org>
2020-09-17 11:00:14 -0500

    lmao

--- twocam.sh
+++ twocam.sh
@@ -2,8 +2,8 @@
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-ffmpeg -f video4linux2 -r 25 -s 640x480 -i /dev/video0 black1.mp4 \
-       -f video4linux2 -r 25 -s 640x480 -i /dev/video2 blue1.mp4 \
+ffmpeg -f video4linux2 -r 25 -s 640x480 -i /dev/video0 black1.avi \
+       -f video4linux2 -r 25 -s 640x480 -i /dev/video2 blue1.avi \
 
 
 
