98e8c8f712d5ea38e38391346c139fb9b6003af2
Daniel <dbruer22@bsmschool.org>
2020-09-25 11:38:09 -0500

    burh

--- cam1nosound.sh
+++ cam1nosound.sh
@@ -1,8 +1,6 @@
--i anullsrc=channel_layout=stereo:sample_rate=44100
-
 ffmpeg \
--f alsa -i default:CARD=ConferenceCam\
--f v4l2 -i /dev/video0 \
+-f alsa -i plughw:CARD=U0x46d0x825,DEV=0 \
+-f v4l2 -i /dev/video2 \
 -c:v libx264 -pix_fmt yuv420p -preset ultrafast -g 10 -b:v 1200 \
 -bufsize 512k \
 -acodec libmp3lame -ar 44100 \
