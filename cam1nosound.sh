d8b160512fb9dd9df629dc128f40290ebe9f1ef9
Daniel <dbruer22@bsmschool.org>
2020-09-25 11:01:39 -0500

    trying the null aduio`

---
+++ cam1nosound.sh
@@ -0,0 +1,11 @@
+ffmpeg \
+-f alsa -i anullsrc=channel_layout=stereo:sample_rate=44100 \
+-f v4l2 -i /dev/video0 \
+-c:v libx264 -pix_fmt yuv420p -preset ultrafast -g 10 -b:v 1200 \
+-bufsize 512k \
+-acodec libmp3lame -ar 44100 \
+-threads 2 -qscale 3 \
+-b:a 128K \
+-r 5 \
+-s 640x480 \
+-f flv rtmp://a.rtmp.youtube.com/live2/uy16-g4ze-j95j-qtjm-2733
