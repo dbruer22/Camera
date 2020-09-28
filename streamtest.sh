313170f2b6a93eb2d3426b4152739edf3a5d4a97
Daniel <dbruer22@bsmschool.org>
2020-09-28 11:45:27 -0500

    i cart poc

--- streamtest.sh
+++ streamtest.sh
@@ -2,7 +2,7 @@ ffmpeg \
 -thread_queue_size 1536 \
 -f alsa -guess_layout_max 0 -i default:CARD=ConferenceCam \
 -thread_queue_size 512 \
--f v4l2 -i /dev/video4 \
+-f v4l2 -i /dev/video0 \
 -c:v libx264 -pix_fmt yuv420p -preset ultrafast -g 8 -b:v 2500k -maxrate 2500k \
 -bufsize 32k -strict experimental \
 -acodec libmp3lame -ar 44100 \
