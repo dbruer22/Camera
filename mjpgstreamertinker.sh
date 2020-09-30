344f02ded1614065eafd6e9227e945faaf4a8db7
Daniel <dbruer22@bsmschool.org>
2020-09-30 11:47:25 -0500

    10x10

--- mjpgstreamertinker.sh
+++ mjpgstreamertinker.sh
@@ -1,1 +1,1 @@
-/usr/local/bin/mjpg_streamer -i "/usr/local/lib/mjpg-streamer/input_uvc.so -n -f 60 -r 300x300 -d /dev/video2" -o "/usr/local/lib/mjpg-streamer/output_http.so -p 8090 -w /usr/local/share/mjpg-streamer/www "
+/usr/local/bin/mjpg_streamer -i "/usr/local/lib/mjpg-streamer/input_uvc.so -n -f 60 -r 10x10 -d /dev/video2" -o "/usr/local/lib/mjpg-streamer/output_http.so -p 8090 -w /usr/local/share/mjpg-streamer/www "
