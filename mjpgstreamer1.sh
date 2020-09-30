0da1f71a24a3936b05f8453bc816a00f0195b2d9
Daniel <dbruer22@bsmschool.org>
2020-09-30 11:37:20 -0500

    ithis is saving

--- mjpgstreamer1.sh
+++ mjpgstreamer1.sh
@@ -1,1 +1,1 @@
-/usr/local/bin/mjpg_streamer -i "/usr/local/lib/mjpg-streamer/input_uvc.so -n -f 10 -r 1280x720 -d /dev/vide0" -o "/usr/local/lib/mjpg-streamer/output_http.so -p 8070 -w /usr/local/share/mjpg-streamer/www "
+/usr/local/bin/mjpg_streamer -i "/usr/local/lib/mjpg-streamer/input_uvc.so -n -f 10 -r 1280x720 -d /dev/video0" -o "/usr/local/lib/mjpg-streamer/output_http.so -p 8070 -w /usr/local/share/mjpg-streamer/www "
